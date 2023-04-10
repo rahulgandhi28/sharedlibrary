bdf5926866df3d4c79e9bc8d3a9c47c67399bb5c
rahulgandhi28 <72257523+rahulgandhi28@users.noreply.github.com>
2023-04-10 13:52:25 +0530

    Update calculator.groovy

--- vars/calculator.groovy
+++ vars/calculator.groovy
@@ -5,15 +5,15 @@ def add(x,y)
 
 def sub(x,y)
 {
-  echo "subtraction of $(x) and $(y) is $(x-y)" 
+   echo "subtraction of ${x} and ${y} is ${x-y}"
 }
 
 def multiply(x,y)
 {
-  echo "multiply of $(x) and $(y) is $(x*y)" 
+   echo "multiplication of ${x} and ${y} is ${x*y}"
 }
 
 def divide(x,y)
 {
-  echo "division of $(x) and $(y) is $(x/y)" 
+   echo "division of ${x} and ${y} is ${x/y}"
 }
